4ae46efa108184a31c8f453486bdf9f034fd9c7c
valerie <valerie.douglas@openlogic.com>
2014-05-28 14:00:03 -0600

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firstapp4::Application.config.secret_key_base = 'a1083fcd96838ad0af4c62de8a2c30cbb5aa3181030d9d3b4cdfe44a9a21d26684a815c39f905181a0b90e854624f52de9be1602ee83257854f1926326d5616a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Firstapp4::Application.config.session_store :cookie_store, key: '_firstapp4_session'
